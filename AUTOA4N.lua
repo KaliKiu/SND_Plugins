1c256e22af3f41bea11e4bcc8b12f695ba6bd10e
Mika Studer <Mika.Studer@students.unibe.ch>
2026-05-04 03:39:21 +0200

    arr

---
+++ AUTOA4N.lua
@@ -0,0 +1,106 @@
+local LoopsToRun = 300
+
+local A4N_ID = 115
+local NORMAL = 1
+
+local function SimpleWait(seconds)
+    yield("/wait " .. seconds)
+end
+
+local function KaliLog(loggin)
+    Dalamud.Log("[SND_AUTA4N]" .. loggin)
+end
+
+local function Setup()
+    while true do
+        if not Instances.DutyFinder.IsUnrestrictedParty then
+            KaliLog("RestrictedParty.. pls unsync")
+            SimpleWait(NORMAL)
+        else
+            KaliLog("UnrestrictedParty")
+            SimpleWait(NORMAL)
+            break
+        end
+    end
+end
+
+local function Check()
+    while true do
+        local qs = Instances.DutyFinder.QueueState
+        if not (qs and qs.InConten) then
+             KaliLog("Waiting to enter ..")
+             SimpleWait(NORMAL)
+        else
+            KaliLog("InContent .. start fight")
+            break
+        end
+    end
+    yield("/echo [SND] Timer finished. Cleaning up...")
+    yield("/vnavmesh moveto -2 10.5 -5.5")
+    SimpleWait(1)
+    yield("/vnavmesh moveto 2 10.5 -5.5")
+    SimpleWait(1)
+    yield("/rotation off")
+    SimpleWait(2)
+    yield("/pdfleave") 
+    yield("/echo [SND] Leaving... Waiting for loading screen")
+end
+
+local function Q()
+    Setup()
+    Instances.DutyFinder:QueueDuty(115)
+    while not Addons.GetAddon("ContentsFinderConfirm").Ready do
+        SimpleWait(NORMAL)
+        KaliLog("waiting..")
+    end
+    SimpleWait(NORMAL)
+    KaliLog("Entering Instance..")
+    yield("/pcall ContentsFinderConfirm true 8")
+--entering is kinda as.. gotta fix that
+    SimpleWait(10)
+end
+
+local function Fight()
+    yield("/rotation manual")
+    yield("/vnavmesh moveto -0.0 10.5 -8.4")
+    SimpleWait(2)
+    local rawTime = InstancedContent.ContentTimeLeft
+    local time = math.floor(rawTime)
+    for i = 1, 300 do
+        yield("/tenemy")
+        local target = Entity.Target
+        rawTime = InstancedContent.ContentTimeLeft
+        time = math.floor(rawTime)
+        if (target or time >5365)  then
+            Dalamud.Log("Fighting.. ")
+        else
+            Dalamud.Log("NoTarget .. EndFight()")
+            break
+        end
+        SimpleWait(NORMAL)
+        if time < 5350 then
+            break
+        end
+        KaliLog(time)
+    end
+end
+
+local function EndFight()
+    SimpleWait(2)
+    yield("/vnavmesh moveto -2 10.5 -5.5")
+    SimpleWait(NORMAL)
+    yield("/vnavmesh moveto 2 10.5 -5.5")
+    SimpleWait(NORMAL)
+    yield("/rotation off")
+    SimpleWait(3)
+    InstancedContent.LeaveCurrentContent()
+end
+
+-- ==========================================
+for i = 1, LoopsToRun do
+    Q()
+    KaliLog("RUN " .. LoopsToRun)
+    Fight()
+    EndFight()
+end
+KaliLog("---.FINISHED.---" .. LoopsToRun)
---
+++ AUTOTURN.lua
@@ -0,0 +1,136 @@
+--AUTHOR KALI KIMURA
+
+--ITEMS ID + COUNT
+local Tarnished = {
+    TGB = {id=12680, count},
+    TGC = {id=12676, count},
+    TGSP = {id=12677, count},
+    TGSH = {id=12675, count},
+    TGP = {id=12678, count}
+}
+
+--CONSTANTS
+local INVENTORY_MIN_SPACE = 40
+local NORMAL = 0.4
+local ITEM_MIN_COUNT = 60
+local run = true
+
+local function SimpleWait(seconds)
+    yield("/wait " .. seconds)
+end
+
+local function KaliLog(loggin)
+    Dalamud.Log("[SND_AE]" .. loggin)
+end
+
+local function Pre()
+    for key, item in pairs(Tarnished) do
+        item.count = Inventory.GetItemCount(item.id)
+        KaliLog(key .. " count: " .. item.count)
+    end
+    local meow = 0;
+    for key, item in pairs(Tarnished) do
+        if item.count > meow then
+            if item.id == 12680 then
+                goto continue
+            end
+            meow = item.count
+        end
+        ::continue::
+    end
+    if meow < 30 then
+        KaliLog("Not enough items")
+        run = false
+        yield("/snd stop AUTO")
+    end
+
+    if Inventory.GetFreeInventorySlots() < INVENTORY_MIN_SPACE then
+        KaliLog("Not enough space in Inventory")
+        return false
+    end
+    return true
+end
+
+local function Tp()
+    KaliLog("Traveling to Idyllshire...")
+    IPC.Lifestream.Teleport(75, 0)
+
+    while IPC.Lifestream.GetRealTerritoryType() ~= 478 do
+        SimpleWait(NORMAL)
+        KaliLog("waiting..")
+    end
+    SimpleWait(5)
+end
+
+local function SabinaSetup()
+    KaliLog("Walking to Sabina...")
+    yield("/vnavmesh moveto -19.45 211 -36.3")
+    SimpleWait(10)
+
+    while Player.IsMoving do
+        KaliLog("Walk..")
+        SimpleWait(1)
+    end
+    yield("/target Sabina")
+    yield("/interact")
+    while not Addons.GetAddon("SelectIconString").Ready do
+        SimpleWait(NORMAL)
+        KaliLog("waiting..")
+    end
+    yield("/callback SelectIconString true 0")
+    while not Addons.GetAddon("SelectString").Ready do
+        SimpleWait(NORMAL)
+        KaliLog("waiting..")
+    end
+end
+
+local function Exchange(index)
+    KaliLog("Exchange " .. index .. "meow")
+    local cases = {
+        [0] = 22,
+        [1] = 13,
+        [2]  =17,
+    }
+    local runs = cases[index]
+    yield("/callback SelectString true ".. index)
+    while not Addons.GetAddon("ShopExchangeItem").Ready do
+        SimpleWait(NORMAL)
+        KaliLog("waiting..")
+    end
+    SimpleWait(NORMAL)
+    for i = 2, runs do
+        yield("/callback ShopExchangeItem true 0 " .. i .. " 1")
+        SimpleWait(NORMAL)
+        yield("/click ShopExchangeItemDialog Exchange")
+        SimpleWait(1)
+    end
+    yield("/pcall ShopExchangeItem true -1")
+    SimpleWait(3)
+end
+
+local function SetupSerpent()
+    yield("/send ESCAPE")
+    SimpleWait(2)
+    yield("/autoretainer deliver")
+    SimpleWait(90)
+end
+
+local function RunSeals()
+    yield("/send ESCAPE")
+    SimpleWait(2)
+    SetupSerpent()
+end
+
+while(run) do
+    if Pre() then
+        Tp()
+        SabinaSetup()
+        Exchange(0)
+        Exchange(1)
+        Exchange(2)
+        KaliLog("FINISHED")
+        RunSeals()
+    end
+end
+
+
